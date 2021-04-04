21badbe4ec2cf017f168fc9749f0c9fc748daa8b
Alexandra Ionel <alexandra.ionel@dialogdata.de>
2021-04-05 00:41:37 +0300

    about us

--- src/app/modules/home/components/about-us/about-us.component.ts
+++ src/app/modules/home/components/about-us/about-us.component.ts
@@ -12,42 +12,42 @@ export class AboutUsComponent implements OnInit {
       name: 'Radu Marinescu',
       role: 'Great Master',
       description: '',
-      image: '',
+      image: 'assets/radu.jpg',
       contact: ''
     },
     {
       name: 'Mario Rivis',
       role: 'Small Master',
       description: '',
-      image: '',
+      image: 'assets/mario.jpg',
       contact: ''
     },
     {
       name: 'Darius Nagy',
       role: 'Sensei',
       description: '',
-      image: '',
+      image: 'assets/darius.jpg',
       contact: ''
     },
     {
       name: 'Alexandra Ionel',
       role: 'Frontend Guru',
       description: '',
-      image: '',
+      image: 'assets/ale.PNG',
       contact: ''
     },
     {
       name: 'Andy Molin',
       role: 'The Virtuous One',
       description: '',
-      image: '',
+      image: 'assets/andy.jpg',
       contact: ''
     },
     {
       name: 'Denis Feier',
-      role: 'to be decided',
+      role: 'The New One',
       description: '',
-      image: '',
+      image: 'assets/denis.jpg',
       contact: ''
     },
   ];
