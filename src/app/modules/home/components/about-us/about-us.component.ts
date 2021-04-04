0c8308da61709db7219b9539dfa94cb521c89ae3
Alexandra Ionel <alexandra.ionel@dialogdata.de>
2021-04-04 22:41:09 +0300

    about us

--- src/app/modules/home/components/about-us/about-us.component.ts
+++ src/app/modules/home/components/about-us/about-us.component.ts
@@ -7,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  devs: any[] = [
+    {
+      name: 'Radu Marinescu',
+      role: 'Great Master',
+      description: '',
+      image: '',
+      contact: ''
+    },
+    {
+      name: 'Mario Rivis',
+      role: 'Small Master',
+      description: '',
+      image: '',
+      contact: ''
+    },
+    {
+      name: 'Darius Nagy',
+      role: 'Sensei',
+      description: '',
+      image: '',
+      contact: ''
+    },
+    {
+      name: 'Alexandra Ionel',
+      role: 'Frontend Guru',
+      description: '',
+      image: '',
+      contact: ''
+    },
+    {
+      name: 'Andy Molin',
+      role: 'The Virtuous One',
+      description: '',
+      image: '',
+      contact: ''
+    },
+    {
+      name: 'Denis Feier',
+      role: 'to be decided',
+      description: '',
+      image: '',
+      contact: ''
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
